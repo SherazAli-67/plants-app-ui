60b20fb088b126e69a877f8cdc69c92ea3a19878
sheraz-ali <soomrosheraz054@gmail.com>
2026-08-17 17:30:32 +0500

    Plants app development
    1. Icons added

---
+++ lib/core/app_icons.dart
@@ -0,0 +1,13 @@
+class AppIcons {
+  static const _iconsPaths = 'assets/icons/';
+
+  //Svgs
+  static const icDrawer = '${_iconsPaths}ic_menu.svg';
+  static const icAdd = '${_iconsPaths}ic_add.svg';
+  static const icNext = '${_iconsPaths}ic_next.svg';
+  static const icRemove = '${_iconsPaths}ic_remove.svg';
+  static const icDelete = '${_iconsPaths}ic_delete.svg';
+
+  //Pngs
+  static const palmTree = '${_iconsPaths}palm_tree.png';
+}
