53c5973463c0782be9dbcc569c90da05f2ecf42e
sheraz-ali <soomrosheraz054@gmail.com>
2026-08-17 16:38:53 +0500

    Plants app development
    1. Colors set

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryGreenColor = Color(0xff3D5308);
+  static const greyColor = Color(0xff8B8B8B);
+  static const greyBgColor = Color(0xffE1E5DE);
+  static const whiteColor = Colors.white;
+}
